83964db11c0b66efe7c50013275a0c87f02a7585
Mac_Developer <araozangel842@gmail.com>
2022-02-28 16:15:22 +0200

    List of products

--- pages/api/production/index.js
+++ pages/api/production/index.js
@@ -5,11 +5,25 @@ export default async function handler(req, res) {
 
 	switch (method) {
 		case 'GET':
-			return res.status(200).json('Getting products');
+			try {
+				const [result] = await pool.query('SELECT * FROM product');
+				console.log(result);
+				return res.status(200).json(result);
+			} catch (error) {
+				console.log(error);
+			}
 		case 'POST':
-			const { name, description, price } = body;
-			const [result] = await pool.query('INSERT INTO product SET ?', { name, description, price });
-			console.log(result);
-			return res.status(200).json({ name, description, price, id: result.insertId });
+			try {
+				const { name, description, price } = body;
+				const [result] = await pool.query('INSERT INTO product SET ?', {
+					name,
+					description,
+					price,
+				});
+				console.log(result);
+				return res.status(200).json({ name, description, price, id: result.insertId });
+			} catch (error) {
+				console.log(error);
+			}
 	}
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import axios from 'axios';
 import { ProductForm } from '../components/ProductForm';
 
-export default function Home() {
+export default function Home({ products }) {
+	console.log(products);
 	return (
 		<div className="text-3xl font-bold underline">
 			<ProductForm />
+			<h3>Hola</h3>
+			{products.map((p) => (
+				<div key={p.id}>
+					<h1>{p.name}</h1>
+					<h2>{p.description}</h2>
+					<h3>{p.price}</h3>
+				</div>
+			))}
 		</div>
 	);
 }
+
+export const getServerSideProps = async (context) => {
+	const res = await axios.get('http://localhost:3000/api/production');
+	console.log(res.data);
+	return {
+		props: {
+			products: res.data,
+		},
+	};
+};
